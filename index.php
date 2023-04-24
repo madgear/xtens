41eaf2f9f9e7c083d05401d3c1b2681d92708890
madgear <anthony.monastrial@gmail.com>
2023-04-24 11:58:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 setInterval(() => {
   console.log("Extension running in the background...");
 }, 5000);
+
+
+// Get the URLs of all open tabs and log them
+function logAllUrls() {
+  chrome.tabs.query({}, tabs => {
+    tabs.forEach(tab => {
+      const url = tab.url;
+      console.log(`Tab URL: ${url}`);
+    });
+  });
+}
+
+// Log all URLs every 5 seconds
+setInterval(logAllUrls, 5000);
+
+
+
+
 <?php
 echo "elow";
 
